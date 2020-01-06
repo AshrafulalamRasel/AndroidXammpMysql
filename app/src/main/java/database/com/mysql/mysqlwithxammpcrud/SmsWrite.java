926601f236359d4c15d05219b162cb3e12172ddf
Rasel <ashrafulrasel21@gmail.com>
2020-01-07 01:54:04 +0600

    fixed code

--- app/src/main/java/database/com/mysql/mysqlwithxammpcrud/SmsWrite.java
+++ app/src/main/java/database/com/mysql/mysqlwithxammpcrud/SmsWrite.java
@@ -2,14 +2,19 @@ package database.com.mysql.mysqlwithxammpcrud;
 
 import android.Manifest;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -19,9 +24,9 @@ import android.widget.Toast;
 public class SmsWrite extends AppCompatActivity {
 
     EditText phoNumber,smsText;
-    Button sendbtn;
+    Button sendbtn,phoneBtn;
     private final static int SEND_SMS_PERMISSION_REQ=1;
-
+    private static final int RESULT_PICK_CONTACT =1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +35,7 @@ public class SmsWrite extends AppCompatActivity {
         phoNumber = findViewById(R.id.phoneNumber);
         smsText =  findViewById(R.id.wrireText);
         sendbtn = findViewById(R.id.send);
+        phoneBtn = findViewById(R.id.phoneNumberCollect);
 
 
         if(checkPermission(Manifest.permission.SEND_SMS))
@@ -66,6 +72,16 @@ public class SmsWrite extends AppCompatActivity {
             }
         });
 
+
+        phoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent (Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult (in, RESULT_PICK_CONTACT);
+                Toast.makeText(SmsWrite.this,"fgdgd",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private boolean checkPermission(String sendSms) {
@@ -84,6 +100,42 @@ public class SmsWrite extends AppCompatActivity {
                     sendbtn.setEnabled(true);
                 }
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        if(resultCode==RESULT_OK)
+        {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked (data);
+                    break;
+            }
+        }
+        else
+        {
+            Toast.makeText (this, "Failed To pick contact", Toast.LENGTH_SHORT).show ();
+        }
+    }
+
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+
+        try {
+            String phoneNo = null;
+            Uri uri = data.getData ();
+            cursor = getContentResolver ().query (uri, null, null,null,null);
+            cursor.moveToFirst ();
+            int phoneIndex = cursor.getColumnIndex (ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+            phoneNo = cursor.getString (phoneIndex);
+
+            phoNumber.setText (phoneNo);
+
+
+        } catch (Exception e) {
+            e.printStackTrace ();
         }
     }
 }
